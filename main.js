a6649784a20336c1af740fa0c76ee2bf37edafbe
krumbhta <tabea.reinhold@gmail.com>
2022-06-09 13:00:57 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+class Buch {
+    constructor(autor, exemplare, seitenzahl, vorhanden){
+       this.autor = autor
+        this.exemplare = exemplare
+        this.seitenzahl = seitenzahl
+        this.vorhanden = vorhanden
+    }
+    
+
+}
