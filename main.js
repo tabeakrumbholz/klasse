86cdc442473b79e9e78fba616ce7cf1a92fe6056
krumbhta <tabea.reinhold@gmail.com>
2022-06-20 14:58:03 +0200

    function verfügbar bearbeitet

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 class Buch {
-    constructor(autor, exemplare, seitenzahl, vorhanden){
+    constructor(autor, exemplare, seitenzahl, verlag){
        this.autor = autor
         this.exemplare = exemplare
         this.seitenzahl = seitenzahl
-        this.vorhanden = vorhanden
+        this.verlag = hersteller
     }
     
+
+}
+
+new Buch ("Maze Runner" , "7" , "400" , "Chicken House")
+
+function verfügbar() {
+    if (this.exemplare > 0){
+    console.log("verfügbar");
+    }
+    if (this.exemplare === 0){
+        console.log("nicht mehr verfügbar")
+    }
+}
+
+function verkauft() {
+
 
 }
